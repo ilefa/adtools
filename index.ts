c36d7f108ee99c4bb3d2bf24b464008126af229d
Mike M <mikester70387@gmail.com>
2022-09-29 14:36:01 -0400

    Added more robust functionality

--- index.ts
+++ index.ts
@@ -18,7 +18,8 @@ import { promisify } from 'util';
  */
 export const bind = async (url: string, username: string, password: string, baseDN: string): Promise<ActiveDirectory> => {
     let ad = new ActiveDirectory({
-        url, baseDN, username, password,
+        url, baseDN,
+        username, password,
         attributes: {
             user: ['*'],
             group: ['*']
@@ -46,6 +47,39 @@ export const bind = async (url: string, username: string, password: string, base
  */
 export const find = async (directory: ActiveDirectory, query: string | {}, err?: (error: Error) => void) =>
     await promisify(directory.find.bind(directory))(query).catch(err || (() => null));
+
+/**
+ * Performs an LDAP query in order to find any OUs
+ * with the specified name in the ActiveDirectory instance.
+ * 
+ * @param directory the active directory instance
+ * @param name the name of the OU to find
+ * @param err an optional error handler
+ * 
+ * @throws if the OU is not found
+ * @returns the OU object, if found
+ */
+export const findOU = async (directory: ActiveDirectory, name: string, err?: (error: Error) => void) => {
+    let results = await find(directory, `(&(objectClass=organizationalUnit)(ou=${name}))`, err);
+    if (!results) return null;
+    return results.other[0];
+}
+
+/**
+ * Performs an LDAP query in order to find all OUs under
+ * a given base DN in the ActiveDirectory instance.
+ * 
+ * @param directory the active directory instance
+ * @param baseDN the search base DN to use
+ * @param err an optional error handler
+ * 
+ * @returns the OUs found under the base DN
+ */
+export const findOUs = async (directory: ActiveDirectory, baseDN?: string, err?: (error: Error) => void) => {
+    let results = await swapBaseThen<any>(directory, baseDN, async () => await find(directory, '(objectClass=organizationalUnit)', err));
+    if (!results) return null;
+    return results.other;
+}
 
 /**
  * Attempts to search ActiveDirectory for the specified
@@ -95,4 +129,30 @@ export const findComputer = async (directory: ActiveDirectory, name: string, err
     let results = await find(directory, `(&(objectClass=computer)(cn=${name}))`, err);
     if (!results) return null;
     return results.other[0];
+}
+
+/**
+ * Performs an LDAP query on the specified ActiveDirectory
+ * in order to find all computer objects in a specified OU.
+ * 
+ * @param directory the active directory instance
+ * @param dn the distinguished name of the OU to search
+ * @param err an optional error handler
+ * 
+ * @throws if the OU is not found
+ * @returns the machines' associated AD objects, if found
+ */
+export const findComputersByOU = async (directory: ActiveDirectory, dn: string, err?: (error: Error) => void) => {
+    let results = await swapBaseThen<any>(directory, dn, async () => await find(directory, '(objectClass=computer)', err));
+    if (!results) return null;
+    return results.other;
+}
+
+// Hacky way to change the base DN of the current ActiveDirectory instance while inflight.
+const swapBaseThen = async <T>(directory: ActiveDirectory, target: string, execute: () => Promise<T>) => {
+    let original = directory.baseDN;
+    directory.baseDN = target;
+    let results = await execute();
+    directory.baseDN = original;
+    return results;
 }
